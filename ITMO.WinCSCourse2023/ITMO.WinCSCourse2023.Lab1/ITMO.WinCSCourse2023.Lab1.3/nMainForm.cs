a419b6b70aeca301596cec3e5b183e75a75b1dcd
Владислав Карелин <vladislav5656565656@gmail.com>
2023-05-25 19:16:01 +0300

    L1Z3

---
+++ ITMO.WinCSCourse2023/ITMO.WinCSCourse2023.Lab1/ITMO.WinCSCourse2023.Lab1.3/nMainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.WinCSCourse2023.Lab1._3
+{
+    public partial class nMainForm : ITMO.WinCSCourse2023.Lab1._3.MainForm
+    {
+        public nMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
