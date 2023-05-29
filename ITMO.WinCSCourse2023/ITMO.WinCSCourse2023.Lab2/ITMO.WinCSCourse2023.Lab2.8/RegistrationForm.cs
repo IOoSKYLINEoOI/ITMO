05c30ec72ffdb74a341af6233ae2a52ba20d32ec
Владислав Карелин <vladislav5656565656@gmail.com>
2023-05-29 19:43:49 +0300

    L2Z8

---
+++ ITMO.WinCSCourse2023/ITMO.WinCSCourse2023.Lab2/ITMO.WinCSCourse2023.Lab2.8/RegistrationForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ITMO.WinCSCourse2023.Lab2._8
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                Label lbl = new Label();
+                lbl.Location = new System.Drawing.Point(16, 110);
+                lbl.Size = new System.Drawing.Size(32, 23);
+                lbl.Name = "labelll";
+                lbl.TabIndex = 2;
+                lbl.Text = "PIN2";
+                groupBox1.Controls.Add(lbl);
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(96, 110);
+                txt.Size = new System.Drawing.Size(184, 20);
+                txt.Name = "textboxx";
+                txt.TabIndex = 1;
+                txt.Text = "";
+                groupBox1.Controls.Add(txt);
+
+                txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            }
+            else
+            {
+                int lcv;
+                lcv = groupBox1.Controls.Count;// определяется количество элементов
+
+                while (lcv > 4)
+                {
+                    groupBox1.Controls.RemoveAt(lcv - 1);
+                    lcv -= 1;
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле Name не может содержать цифры");
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле PIN не может содержать буквы");
+            }
+        }
+    }
+}
