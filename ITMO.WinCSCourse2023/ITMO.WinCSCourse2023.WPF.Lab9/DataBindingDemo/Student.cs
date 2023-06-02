6dcbe79d33b33fdc58fd6d39eb316f5cad66f7e9
Владислав Карелин <vladislav5656565656@gmail.com>
2023-06-02 03:21:09 +0300

    WPF.L9

---
+++ ITMO.WinCSCourse2023/ITMO.WinCSCourse2023.WPF.Lab9/DataBindingDemo/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Globalization;
+
+namespace DataBindingDemo
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+
+    public class StudentList : ObservableCollection<Student>
+    {
+    }
+
+
+}
