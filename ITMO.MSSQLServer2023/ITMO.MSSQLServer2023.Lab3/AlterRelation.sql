bf8a9e38e1f1bd8b9c0c45e01a2b17156d265a69
Владислав Карелин <vladislav5656565656@gmail.com>
2023-07-26 00:56:15 +0300

    SQL.L3

---
+++ ITMO.MSSQLServer2023/ITMO.MSSQLServer2023.Lab3/AlterRelation.sql
@@ -0,0 +1,8 @@
+USE ApressFinancial
+GO
+
+ALTER TABLE TransactionDetails.Transactions
+WITH NOCHECK
+ADD CONSTRAINT FK_Transactions_Shares FOREIGN KEY (RelatedShareId)
+	REFERENCES ShareDetails.Shares(ShareID)
+GO
---
+++ ITMO.MSSQLServer2023/ITMO.MSSQLServer2023.Lab3/CreateTeableTemplate.sql
@@ -0,0 +1,17 @@
+-- =========================================
+-- Create table template
+-- =========================================
+USE ApressFinancial
+GO
+
+IF OBJECT_ID('TransactionDetails.TransactionTypes', 'U') IS NOT NULL
+  DROP TABLE TransactionDetails.TransactionTypes
+GO
+
+CREATE TABLE TransactionDetails.TransactionTypes
+(
+	TransactionTyesId int IDENTITY (1,1) NOT NULL, 
+	TransactionDescription nvarchar(30) NOT NULL, 
+	CreditType bit NOT NULL
+)
+GO
