c479002a7a31152e78af951d4eb3fc3f28e5e929
Владислав Карелин <vladislav5656565656@gmail.com>
2023-10-04 17:02:08 +0300

    ADO.NET.L4

---
+++ ITMO.ADO.NET2023/ITMO.ADO.NET2023.Lab4/ITMO.ADO.NET2023.Lab4.4/DataAdapterProgram/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAdapterProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter1 = new SqlDataAdapter("SELECT * FROM Customers", NorthwindConnection);
+            NorthwindDataset.Tables.Add(CustomersTable);
+            SqlDataAdapter1.Fill(NorthwindDataset.Tables["Customers"]);
+            dataGridView1.DataSource = NorthwindDataset.Tables["Customers"];
+
+            SqlCommandBuilder commands = new SqlCommandBuilder(SqlDataAdapter1);
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataset.EndInit();
+            SqlDataAdapter1.Update(NorthwindDataset.Tables["Customers"]);
+        }
+    }
+}
